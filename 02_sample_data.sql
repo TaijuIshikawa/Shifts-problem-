d4468cf5f2b04bb4f792bddfb3ec656c6049b237
TaijuIshikawa <i.0929ta110@gmail.com>
2026-09-06 17:06:21 +0200

    Refactor SQL data insertion for clarity and organization
    Updated SQL insert statements for stores, employees, and daily shifts with improved formatting and additional comments.

--- 02_sample_data.sql
+++ 02_sample_data.sql
@@ -1,19 +1,38 @@
 -- 1. STORES
-INSERT INTO stores(store_name, location) VALUES
-('Doutor Harajuku', 'Harajuku'),
-('Doutor Shibuya', 'Shibuya-Centre')
-('Doutor Takeshita','Takeshita-Street');
+INSERT INTO stores (store_name, location) VALUES
+    ('Doutor Harajuku', 'Harajuku'),
+    ('Doutor Shibuya', 'Shibuya-Centre'),
+    ('Doutor Takeshita', 'Takeshita-Street');
 
 -- 2. EMPLOYEES
-INSERT INTO employees(employee_name, hourly_wage, role, manager_id, condition, store_id) VALUES
-('Taiju Ishikawa', 1750, 'Store Manager', NULL, 'Full-time',3),
-('Ruosuke Hatanaka', 1350, 'Barista', 1, 'Student',3),
-('Chandler McCoy', 1350, 'Barista', 1, 'English', 3),
-('Hyelim Kang', 1400, 'Floater Barista', NULL, 'Cross-Store Support', 2);
+INSERT INTO employees (employee_name, hourly_wage, role, manager_id, condition, store_id) VALUES
+    -- Takeshita Branch 
+    ('Taiju Ishikawa', 1750, 'Store Manager', NULL, 'Full-time, English', 3),
+    ('Ryosuke Hatanaka', 1350, 'Barista', 1, 'Student', 3),
+    ('Chandler McCoy', 1350, 'Barista', 1, 'English', 3),
 
--- 3. SHIFTS
-INSERT INTO daily_shifts(shift_day, store_id, weekly_hour, employee_id) VALUES
-('Everyday',3,65,1)
-( 'Weekend', 3, 20, 2),
-('Weekday', 3, 35, 3), 
-('Weekday', 2, 35, 4);  
+    -- Harajuku Branch
+    ('Yuta Araki', 1400, 'Barista', NULL, 'Cross-Store Support,Weekend', 1),
+    ('Sogo Miyamoto', 1400, 'Barista', NULL, 'Weekend Floater', 1),
+    ('Yusuke Yoshimoto', 1450, 'Senior Barista', NULL, 'Weekend', 1),
+
+    -- Shibuya Branch 
+    ('Inji Che', 1400, 'Barista', NULL, 'Cross-Store Support', 2),
+    ('Momoka Fujioka', 1400, 'Barista', NULL, 'Weekend Support', 2);
+
+-- 3. DAILY SHIFTS 
+INSERT INTO daily_shifts (shift_day, store_id, weekly_hour, employee_id) VALUES
+    ('Everday', 3, 40, 1), -- Taiju Ishikawa
+    ('Weekend', 3, 25, 2), -- Ryosuke Hatanaka
+    ('Weekday', 3, 25, 3), -- Chandler McCoy
+    ('Weekend', 3, 16, 4), -- Yuta Araki 
+    ('Weekend', 3, 12, 5), -- Sogo Miyamoto 
+    ('Weekend', 3, 16, 7), -- Inji Che 
+    ('Weekend', 3, 12, 8), -- Momoka Fujioka 
+    ('Weekday', 1, 30, 6), -- Yusuke Yoshimoto 
+    ('Weekday', 2, 20, 9); -- Hyelim Kang 
+
+
+
+
+
